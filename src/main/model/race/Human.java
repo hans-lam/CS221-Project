9178175fbaf7840345f3bade3e824511a3ce809a
Hans Lam <hans.kt.lam@gmail.com>
2019-10-28 21:51:39 -0700

    Before Deliv 7

--- src/main/model/race/Human.java
+++ src/main/model/race/Human.java
@@ -4,6 +4,13 @@ public class Human extends Races {
     public Human(String abilityScores, String size, String speed, String age, String alignment,
                  String racialTraits, String languages) {
         super(abilityScores, size, speed, age, alignment, racialTraits, languages);
+        this.abilityScores = "All stats +1";
+        this.size = "Medium";
+        this.speed = "30 ft.";
+        this.age = "Humans reach adulthood in their late teens and live less than a century.";
+        this.alignment = "Humans tend toward no particular alignment. The best and the worst are found among them.";
+        this.racialTraits = "";
+        this.languages = "";
     }
 
     @Override
--- src/main/ui/DiceRoll.java
+++ src/main/ui/DiceRoll.java
@@ -6,7 +6,7 @@ import model.dice.DiceSidesException;
 
 public class DiceRoll {
     public static void main(String[] args) {
-        DiceRoller d20 = new DiceRoller(20, 0);
+        DiceRoller d20 = new DiceRoller(6, 8);
         d20.diceRoll();
     }
 }
